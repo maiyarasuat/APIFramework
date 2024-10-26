d434b62b5e5034fa6847353b959cba11e41bb427
Maiyarasu <maiyarasu@Maiyarasus-MacBook-Air.local>
2024-10-26 23:18:51 +0530

    updated dynamicjson test

--- src/test/java/com/qa/api/tests/UserApiTestWithDynamicJsonTest.java
+++ src/test/java/com/qa/api/tests/UserApiTestWithDynamicJsonTest.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.qa.api.base.BaseTest;
+import com.qa.api.constants.AuthType;
 import com.qa.api.utils.StringUtil;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 public class UserApiTestWithDynamicJsonTest extends BaseTest{
 	
@@ -35,6 +40,9 @@ public class UserApiTestWithDynamicJsonTest extends BaseTest{
 			//convert json node into json string
 			String updatedJsonString = mapper.writeValueAsString(userNode);
 			System.out.println("updated json string===>" + updatedJsonString);
+			
+			Response response = restClient.post(BASE_URL_GOREST, "/public/v2/users", updatedJsonString, null, null, AuthType.BEARER_TOKEN, ContentType.JSON);
+			Assert.assertEquals(response.statusCode(), 201);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
